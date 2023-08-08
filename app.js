4ab11c506e80772e68792ae77e89279c5874d970
Fabio <clonefabster7@yahoo.com>
2023-08-08 13:43:50 -0700

    Practicing arrays

---
+++ app.js
@@ -0,0 +1,73 @@
+
+/*
+for (let i = 1; i <= 20; ++i) {
+    if (i % 3 === 0 && i % 5 === 0){
+    console.log('Frontend Simplified')
+}
+    else if (i % 3 === 0) {
+        console.log('Frontend')
+    }
+    else if (i % 5 === 0) {
+        console.log('Simplified')
+    }
+} 
+
+
+const str = "Frontend Simplified"
+
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+}
+
+const sentence = "Hello, world!";
+for (let i = 0; i < sentence.length; ++i) {
+    console.log(sentence[i]);
+}
+
+function sumOfTwoNumbers (a, b) {
+    return a * b
+}
+
+console.log(sumOfTwoNumbers(5, 14))
+
+
+function convertCelsius (celsius) {
+    return celsius * 1.8 + 32 
+}
+
+console.log(convertCelsius(0))
+console.log(convertCelsius(10))
+console.log(convertCelsius(30))
+
+
+const convertCelsius = (celsius) => {
+    return celsius * 1.8 + 32
+}
+
+
+let arr = [1 , 2, 3, 4, 5]
+
+//First element of array
+console.log(arr[0])
+
+//Last element of array
+console.log(arr[arr.length - 1])
+
+//Shorter version of array
+
+let newArr = arr.filter(element => element < 50)
+
+console.log(newArr)
+
+*/
+
+let grades = ['A+', 'A', 'FAIL']
+
+for (let i = 0; i < grades.length; ++i) {
+    console.log(grades[i]);
+}
+
+// let goodGrades = grades.filter(element => element !== 'FAIL')
+
+// console.log(goodGrades) 
+ 
